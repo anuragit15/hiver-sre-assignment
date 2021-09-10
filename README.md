cec2b152e1dc9f520c1d6d65b6c6538bb4f16173
Anurag gandhi <anurag.gandhi@digitas.com>
2021-09-10 18:54:53 +0530

    Add python code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # hiver-sre-assignment
 Assignment for hiver SRE
+
+Part1: Terraform Iac
+
+To run the terraform configuration:
+
+Step1: Install terraform
+Step2: The Aws provider expects a configuration file saved in ~/.aws/credentials, the systax of the file is as below:
+
+[default]
+aws_access_key_id = A*************
+aws_secret_access_key = D***********
+
+Or use any other authentication method as per: https://registry.terraform.io/providers/hashicorp/aws/latest/docs#authentication
+
+Step3: cd terraform-iac
+Step4: terraform apply
+
+Note: There are no input variables expected for the terraform config.
+
+Part2: Python script
+
+To run the Python Script:
+
+Step1: cd python-ec2 
+Step2: pip3 install -r requirements.txt
+Step3: python3 describe-ec2.py
---
+++ python-ec2/describe-ec2.py
@@ -0,0 +1,44 @@
+import boto3
+from prettytable import PrettyTable
+
+session = boto3.session.Session()
+
+ec2_client = session.client(service_name = 'ec2', region_name = "us-east-1")
+
+def get_tags(resourceId):
+    tags = ec2_client.describe_tags(Filters=[
+        {
+            "Name": "resource-id",
+            "Values": [resourceId]
+        },
+        {
+            "Name": "resource-type",
+            "Values": ["instance"]
+        }
+    ])
+    for f in tags['Tags']:
+        if f['Key'] == 'Name':
+            return f['Value']
+            break
+        else:
+            continue
+    else:
+        return("Name tag not found")
+
+reservations = ec2_client.describe_instances(Filters=[
+        {
+            "Name": "instance-state-name",
+            "Values": ["running"]
+        }
+    ]).get("Reservations")
+
+table = PrettyTable(['Name Tag','Instance ID'])
+
+for reservation in reservations:
+    for instance in reservation["Instances"]:
+        if instance["InstanceType"] == 'm5.xlarge':
+          instance_id = instance["InstanceId"]
+          name_tag = get_tags(instance_id)
+          table.add_row([ name_tag , instance_id ])
+
+print(table)
